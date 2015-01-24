bed22da29455d0f5afc58a0e1134f73ae48d9c4e
andresitoo <andymorale@hotmail.com>
2015-01-24 15:42:41 -0500

    cracion e inclucion al body el footer php

---
+++ plantillasphp/footer.php
@@ -0,0 +1,21 @@
+<!-- footer -->
+			<footer>
+				<div class="wrapper">
+					<section class="col-2-3" > <div class="wrap-col">
+						<figure id="crazy"><img src="images/crazy1.png" alt="" ></figure>
+                        <br>
+						<a rel="nofollow" href="pages/beneficios.html" >Beneficios</a> 
+                        <br>
+                        <a rel="nofollow" href="pages/servicios.html" >Servicios</a> 
+					</div></section>
+					<section class="col-1-3"><div class="wrap-col">
+						<h3>Siguenos </h3>
+						<ul id="icons">
+							<li><a href="http://www.facebook.com" target="_blank" class="normaltip" title="Facebook"><img src="images/icon1.gif" alt=""></a></li>
+							<li><a href="http://www.twitter.com" target="_blank" class="normaltip" title="Twitter"><img src="images/icon3.gif" alt=""></a></li>
+						</ul>
+					</div></section>
+				</div>
+				<!-- {%FOOTER_LINK} -->
+			</footer>
+<!-- / footer -->
