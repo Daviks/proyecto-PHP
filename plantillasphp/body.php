714adb72ddf14cdb2af596601f10e9ebc34de3ce
andresitoo <andymorale@hotmail.com>
2015-01-24 15:38:22 -0500

    creacion e inclucion del body al index php

---
+++ plantillasphp/body.php
@@ -0,0 +1,159 @@
+
+<body id="page1">
+<div class="body6">
+	<div class="body1">
+		<div class="body5">
+			<div class="main zerogrid">
+<!-- header -->
+				<header>
+					<h1>
+                        <a href="index.html" id="logo"><img src="images/logoenblanco.png" alt="">
+                        </a>
+                    </h1>
+					<nav>
+                  
+						<ul id="top_nav">
+							<li><a href="pages/ingresar.html" title="Ingresar"><img src="images/icon_1.gif" alt=""></a></li>
+							<li><a href="pages/registrar.html" title="Registrar"><img src="images/icon_2.gif" alt=""></a></li>
+							<li class="end"><a href="pages/ayuda.html" title="Ayuda"><img src="images/icon_3.gif" alt=""></a></li>
+						</ul>
+					</nav>
+					<nav>
+						<ul id="menu">
+							<li class="active"><a href="pages/restaurante.html">Restaurante</a></li>
+							<li><a href="pages/ciudad.html">Ciudad</a></li>
+							<li><a href="pages/comida.html">Comida</a></li>
+                            <li><a href="pages/nuevaMarca.html">Administración</a></li>
+                            <li><a href="pages/nosotros.html">Nosotros</a></li>
+                            
+						</ul>
+					</nav>
+				</header>
+<!-- / header -->
+<!-- content -->
+				<article id="content">
+					<div class="slider_bg">
+						<div class="slider">
+							<ul class="items">
+								<li>
+									<img src="images/ima1.jpg" alt="">
+									<div class="banner">
+										<strong>Café</strong>
+                                        <br/>
+										<p>
+											<span>Deliciosa y refrescante preparación<br>
+											del café elaborada apartir de una base láctea, hielo y deliciosos complemntos.</span>
+										</p>
+									</div>
+								</li>
+								<li>
+									<img src="images/ima2.jpg" alt="">
+									<div class="banner">
+										<strong>Pízza</strong>
+                                        <br/>
+										<p>
+											<span>Para los que le gusta la carne <br>
+											Mozzarellamo, Pepproni, Choricillos y Pork.</span>
+										</p>
+									</div>
+								</li>
+								<li>
+									<img src="images/ima3.jpg" alt="">
+									<div class="banner">
+										<strong>Carne</strong>
+                                        <br/>
+										<p>
+											<span>Delicioso lomo de falda de 150 gr. <br>
+											Una porción de deliciosas papas fritas, ensaladas con vegetales frescos, salsa verde y roja.</span>
+										</p>
+									</div>
+								</li>
+							</ul>
+						</div>
+						<div class="slider-response">
+							<div class="rslides_container">
+								<ul class="rslides" id="slidez">
+									<li><img src="images/ima1.jpg" alt=""></li>
+									<li><img src="images/ima2.jpg" alt=""></li>
+									<li><img src="images/ima3.jpg" alt=""></li>
+								</ul>
+							</div>
+	                    </div>
+					</div>
+					<div class="wrap">
+						<section class="col-1-3"><div class="wrap-col">
+							<div id="contenido">
+								<div class="box">
+                                <div>
+									<h2><span>Bienvenido</span></h2>
+									<figure><img src="images/page1_img1.jpg" alt="" ></figure>
+									<p class="pad_bot1"><a href="http://blog.templatemonster.com/2011/08/01/free-website-template-jquery-slider-typography-restaurant/" class="list_1">Crazy Food<br></a>"Consigue tu comida favorita más Rápido y Sencillo".</p>
+                                    <br/>
+									<a href="pages/restaurante.html" class="button1">Leer Más</a>
+								</div>
+							</div>
+						</div>
+						</div>
+                        </section>
+						<section class="col-1-3"><div class="wrap-col">
+							<div class="box">
+                            
+								<div>
+									<h2><span>Nosotros</span></h2>
+									<figure><img src="images/page1_img2.jpg" alt="" ></figure>
+									<p class="pad_bot1"> Somos "una empresa de jovenes " con visión creativa e innovadora
+en el ámbito de ventas y pedidos en línea para empresas gastronomicas.</p>
+									<a href="pages/nosotros.html" class="button1">Leer Más</a>
+								</div>
+							</div>
+						</div></section>
+						<section class="col-1-3"><div class="wrap-col">
+							<div class="box">
+								<div>
+									<h2><span>Servicios</span></h2>
+									<figure><img src="images/page1_img3.jpg" alt="" ></figure>
+									<ul class="list1 pad_bot1">
+										<li><a href="#">Compra de Comida en Línea</a></li>
+										<li><a href="#">Pedido de comida previo al arrivo del cliente al local</a></li>
+										<li><a href="#">Diversidad de pagos</a></li>
+									</ul>
+									<a href="pages/servicios.html" class="button1">Leer Más</a>
+								</div>
+							</div>
+						</div></section>
+					</div>
+				</article>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="body3">
+
+		<div class="main zerogrid">
+<!-- footer -->
+			<footer>
+				<div class="wrapper">
+					<section class="col-2-3" > <div class="wrap-col">
+						<figure id="crazy"><img src="images/crazy1.png" alt="" ></figure>
+                        <br>
+						<a rel="nofollow" href="pages/beneficios.html" >Beneficios</a> 
+                        <br>
+                        <a rel="nofollow" href="pages/servicios.html" >Servicios</a> 
+					</div></section>
+					<section class="col-1-3"><div class="wrap-col">
+						<h3>Siguenos </h3>
+						<ul id="icons">
+							<li><a href="http://www.facebook.com" target="_blank" class="normaltip" title="Facebook"><img src="images/icon1.gif" alt=""></a></li>
+							<li><a href="http://www.twitter.com" target="_blank" class="normaltip" title="Twitter"><img src="images/icon3.gif" alt=""></a></li>
+						</ul>
+					</div></section>
+				</div>
+				<!-- {%FOOTER_LINK} -->
+			</footer>
+<!-- / footer -->
+		</div>
+
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
