ca1261c31525904157731a52eeb64ad2d10d193d
JDGetsuga <jdavid_wolf@hotmail.com>
2015-01-24 23:11:09 -0500

    Cambio Php registrar

---
+++ plantillasphp/contenidoBodyRegistrar.php
@@ -0,0 +1,52 @@
+<!-- content -->
+<br>
+<br>
+
+			<article id="content">
+				<div class="wrap">
+					<div class="box">
+						<div>
+							<h2 class="letter_spacing">Registrar</h2>
+                            <br>
+							<form id="ContactForm">
+								<div>
+									<div class="wrapper">
+										<span>Nombre:</span>
+										<input type="text" class="input" >
+									</div>
+                                    <br>
+									<div class="wrapper">
+										<span>Apellidos:</span>
+										<input type="text" class="input" >								
+									</div>
+                                                                        <br>
+									<div class="wrapper">
+										<span>Email:</span>
+										<input type="text" class="input" >								
+									</div>                                    <br>
+									<div class="wrapper">
+										<span>Contraseña:</span>
+										<input type="text" class="input" >								
+									</div>
+                                                                       <br>
+									<div class="wrapper">
+										<span>Dirección:</span>
+										<input type="text" class="input" >								
+									</div>
+                                    <br>											                                     <div class="wrapper">
+										<span>Teléfono:</span>
+										<input type="text" class="input" >								
+									</div>
+                                     <br>
+                                    
+									<div class="centrar">
+                                    <br>
+									<a href="#" class="button1" onClick="document.getElementById('ContactForm').submit()">Registrar</a>
+									<a href="#" class="button1" onClick="document.getElementById('ContactForm').reset()">Cancelar</a>		
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</article>
