74a97602ddea5d4ea3080b79c3d7ee0744ae05e2
JDGetsuga <jdavid_wolf@hotmail.com>
2015-01-24 21:46:38 -0500

    Contenido ingresar php

---
+++ plantillasphp/contenidoBodyIngresar.php
@@ -0,0 +1,35 @@
+<!-- content -->
+<br>
+<br>
+
+			<article id="content">
+			<div class="wrap">
+			  <div class="box">
+			    <div>
+			      <h2 class="letter_spacing">Ingresar</h2>
+			      <br>
+			      <form id="ContactForm">
+			        <div>
+			          <div class="wrapper"> <span>Usuario:</span>
+			            <input type="text" class="input" >
+		              </div>
+			          <br>
+			          <div class="wrapper"> <span>Contraseña:</span>
+			            <input type="text" class="input" >
+		              </div>
+			          <div class="centrar"> <br>
+			            <a href="#" class="button1" onClick="document.getElementById('ContactForm').submit()">Aceptar</a> <a href="#" class="button1" onClick="document.getElementById('ContactForm').reset()">Cancelar</a> </div>
+		            </div>
+		          </form>
+		        </div>
+		      </div>
+		  </div>
+      
+      <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+		
+		</article>
