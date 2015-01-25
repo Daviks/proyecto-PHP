a1451904449b0087ebef62b9dcfa530df5c76ea6
JDGetsuga <jdavid_wolf@hotmail.com>
2015-01-24 23:12:23 -0500

    Contenido Registrar

---
+++ plantillasphp/footerBodyRegistrar.php
@@ -0,0 +1,21 @@
+<!-- footer -->
+			<footer>
+				<div class="wrapper">
+					<section class="col-2-3"><div class="wrap-col">
+						<figure id="crazy"><img src="../images/crazy1.png" alt="" ></figure>
+                        <br>
+						<a rel="nofollow" href="../pages/beneficios.html" >Beneficios</a> 
+                        <br>
+                        <a href="../pages/servicios.html">Servicios</a>
+					</div></section>
+					<section class="col-1-3"><div class="wrap-col">
+						<h3>Siguenos </h3>
+						<ul id="icons">
+							<li><a href="#" class="normaltip" title="Facebook"><img src="../images/icon1.gif" alt=""></a></li>
+							<li><a href="#" class="normaltip" title="Twitter"><img src="../images/icon3.gif" alt=""></a></li>
+						</ul>
+					</div></section>
+				</div>
+				<!-- {%FOOTER_LINK} -->
+			</footer>
+<!-- / footer -->
