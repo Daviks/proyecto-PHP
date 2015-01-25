7b0f799f7835e9da84aacbb3cf7019f59ea00c74
JDGetsuga <jdavid_wolf@hotmail.com>
2015-01-24 21:46:14 -0500

    Cambio a php

---
+++ plantillasphp/bodyIngresar.php
@@ -0,0 +1,33 @@
+
+<body id="page1">
+	<div class="body6">
+		<div class="body1">
+			<div class="body5">
+				<div class="main zerogrid">
+
+	<!-- header -->
+				<?php include '../plantillasphp/headerBodyIngresar.php' ; ?>
+	<!-- / header -->
+
+	<!-- content -->
+				<?php include '../plantillasphp/contenidoBodyIngresar.php' ; ?>
+	<!-- / content -->
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="body3">
+			<div class="main zerogrid">
+
+				<!-- footer -->
+
+				<?php include '../plantillasphp/footerBodyIngresar.php' ; ?>
+
+				<!-- /footer -->
+
+			</div>
+	</div>
+	<script type="text/javascript"> Cufon.now(); </script>
+</body>
