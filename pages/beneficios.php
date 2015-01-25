9b78ffe1968d5a5c01739e8cf122a1b08288a00a
JDGetsuga <jdavid_wolf@hotmail.com>
2015-01-25 00:24:45 -0500

    CAmbio php beneficios

---
+++ pages/beneficios.php
@@ -0,0 +1,11 @@
+<?php
+session_start()
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '../plantillasphp/headIndex.php' ; ?>
+<?php include '../plantillasphp/bodybeneficios.php' ; ?>
+
+</html>
---
+++ plantillasphp/bodybeneficios.php
@@ -0,0 +1,33 @@
+
+<body id="page1">
+	<div class="body6">
+		<div class="body1">
+			<div class="body5">
+				<div class="main zerogrid">
+
+	<!-- header -->
+				<?php include 'headerBodyBeneficios.php' ; ?>
+	<!-- / header -->
+
+	<!-- content -->
+				<?php include 'contenidoBodyBeneficios.php' ; ?>
+	<!-- / content -->
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="body3">
+			<div class="main zerogrid">
+
+				<!-- footer -->
+
+				<?php include 'footerBodyBeneficios.php' ; ?>
+
+				<!-- /footer -->
+
+			</div>
+	</div>
+	<script type="text/javascript"> Cufon.now(); </script>
+</body>
