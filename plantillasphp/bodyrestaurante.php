5d918730d22aae002c588d37e94bcedc96405476
Carlos León <carlos.leon.lucas@gmail.com>
2015-01-25 23:22:37 -0500

    division pag restaurantes

---
+++ plantillasphp/bodyrestaurante.php
@@ -0,0 +1,77 @@
+
+<body id="page1">
+<div class="body6">
+	<div class="body1">
+		<div class="body5">
+			<div class="main zerogrid">
+<!-- header -->
+				<header>
+					<h1><a href="../index.html" id="logo"><img src="../images/logoenblanco.png" alt="" ></a></h1>
+					<nav>
+						<ul id="top_nav">
+							<li><a href="../pages/ingresar.html" title="Ingresar"><img src="../images/icon_1.gif" alt=""></a></li>
+							<li><a href="../pages/registrar.html" title="Registrar"><img src="../images/icon_2.gif" alt=""></a></li>
+							<li class="end"><a href="../pages/ayuda.html" title="Ayuda"><img src="../images/icon_3.gif" alt=""></a></li>
+						</ul>
+					</nav>
+					<nav>
+						<ul id="menu">
+							<li class="active"><a href="#">Restaurante</a></li>
+							<li><a href="../pages/ciudad.html">Ciudad</a></li>
+							<li><a href="../pages/comida.html">Comida</a></li>
+                             <li><a href="../pages/nuevaMarca.html">Administración</a></li>
+                            <li><a href="../pages/nosotros.html">Nosotros</a></li>
+						</ul>
+					</nav>
+				</header>
+				<h2 id="titulopag"> Cafeterías </h2> <input  href="#" class="button1" id="seccionbuscar" type="submit" value="Buscar"><br><br> <input  id="seccionbuscar" type="search" name="buscar"><br>
+				
+<!-- / header -->
+	
+				
+				<div id="contenido">
+				
+									<div id="banner_left">
+										<ul id="menu_categorias">
+											<li id="cate">Categorias</li>
+											<li>Café</li>
+											<li>Pizza</li>
+											<li>Mariscos</li>
+											<li>Carnes</li>
+											
+										</ul>
+
+									</div>
+									
+									<div id="banner_right">
+										<img src="../images/dominoes.png" class="imares"  alt="res1" />
+										<img src="../images/kfc.png" class="imares"  alt="res1" />
+										<img src="../images/mc2.png" class="imares"  alt="res1" />
+										  <br/>  <br/>  <br/>  <br/>
+										<a rel="nofollow" href="../pages/ingresar_restaurante.html" >Anúnciate con nosotros aquí...</a>
+											
+										<p>		
+									</div>
+									
+								  <br/> 
+								  
+				</div>
+	
+				
+
+<!-- content -->
+				<article id="content">
+					
+							
+				</article>
+				
+				
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
