4fc2481afcd3695c797425d21da00506993e9238
JDGetsuga <jdavid_wolf@hotmail.com>
2015-01-26 22:27:14 -0500

    eliminacion de ../

--- plantillasphp/headIndex.php
+++ plantillasphp/headIndex.php
@@ -1,9 +1,9 @@
 <title>Ingresar</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link rel="stylesheet" href="../css/estilo.css" type="text/css" media="all">
-<link rel="shortcut icon" href="../images/crazy.ico" />
-<link rel="stylesheet" href="../css/responsiveslides.css" /> 
+<link rel="stylesheet" href="css/estilo.css" type="text/css" media="all">
+<link rel="shortcut icon" href="images/crazy.ico" />
+<link rel="stylesheet" href="css/responsiveslides.css" /> 
 <script type="text/javascript" src="js/jquery-1.6.js" ></script>
 <script type="text/javascript" src="js/cufon-yui.js"></script>
 <script type="text/javascript" src="js/cufon-replace.js"></script>  
