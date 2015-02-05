3591d7b883f51ed5efe7899967369578f0910dbf
Carlos León <carlos.leon.lucas@gmail.com>
2015-02-05 11:31:57 -0500

    ciudad php completo

---
+++ plantillasphp/bodyciudad.php
@@ -0,0 +1,66 @@
+<body id="page1">
+<div class="body6">
+	<div class="body1">
+		<div class="body5">
+			<div class="main zerogrid">
+<!-- header -->
+				<header>
+					<h1><a href="../index.html" id="logo"><img src="../images/logoenblanco.png" alt=""></a></h1>
+					<nav>
+						<ul id="top_nav">
+							<li><a href="../pages/ingresar.html" title="Ingresar"><img src="../images/icon_1.gif" alt=""></a></li>
+							<li><a href="../pages/registrar.html" title="Registrar"><img src="../images/icon_2.gif" alt=""></a></li>
+							<li class="end"><a href="../pages/ayuda.html" title="Ayuda"><img src="../images/icon_3.gif" alt=""></a></li>
+						</ul>
+					</nav>
+					<nav>
+						<ul id="menu">
+							<li><a href="../pages/restaurante.html">Restaurante</a></li>
+							<li class="active"><a href="#">Ciudad</a></li>
+							<li><a href="../pages/comida.html">Comida</a></li>
+                            <li><a href="../pages/nuevaMarca.html">Administración</a></li>
+                            <li><a href="../pages/nosotros.html">Nosotros</a></li>
+                            
+						</ul>
+					</nav>
+				</header>
+				<h2 id="titulopag"> Guayaquil </h2> <input  href="#" class="button1" id="seccionbuscar" type="submit" value="Buscar"><br><br> <input  id="seccionbuscar" type="search" name="buscar"><br>
+				
+				<div id="contenido">
+				
+									<div id="banner_left">
+										<ul id="menu_categorias">
+											<li id="cate">Categorias</li>
+											<li>Café</li>
+											<li>Pizza</li>
+											<li>Mariscos</li>
+											<li>Carnes</li>
+											
+										</ul>
+
+									</div>
+									
+									<div id="banner_right">
+										<img src="../images/Tomate.png" class="imares"  alt="res1">
+										<img src="../images/mcdonalds.png" class="imares"  alt="res1" >
+										<img src="../images/Menestras_Del_Negro_Logo.jpg" class="imares"  alt="res1" >
+												
+										
+									</div>
+								  <br/> 
+				</div>
+	
+				
+<!-- / header -->
+<!-- content -->
+				<article id="content">							
+				</article>
+
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include '../plantillasphp/footRCC.php' ; ?>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
