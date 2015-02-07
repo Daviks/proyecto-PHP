1314085faf6112d70217d28f33871538182daaf8
Arturo <arturonrg@gmail.com>
2015-02-06 19:37:33 -0500

    body para página nueva marca

---
+++ plantillasphp/bodyNuevaMarca.php
@@ -0,0 +1,87 @@
+<!-- content -->
+<?php include '../plantillasphp/headerBodyNuevaMarca.php' ; ?>
+
+<!-- content -->
+<br>
+<br>
+
+			<article id="content">
+				<div class="wrap">
+					<div class="box">
+						<div>
+							<h2 class="letter_spacing">Registrar nueva marca</h2>
+                            <br>
+							<form id="ContactForm">
+								<div>
+									<div class="wrapper">
+										<span>Razón social:</span>
+										<input type="text" class="input" >
+									</div>
+                                    <br>
+									<div class="wrapper">
+										<span>RUC:</span>
+										<input type="text" class="input" >								
+									</div>                                    <br>
+									<div class="wrapper">
+										<span>Contraseña:</span>
+										<input type="text" class="input" >								
+									</div>
+                                                                       <br>
+									<div class="wrapper">
+										<span>Dirección:</span>
+										<input type="text" class="input" >								
+									</div>
+                                    <br>											                                     <div class="wrapper">
+										<span>Teléfono:</span>
+										<input type="text" class="input" >								
+									</div>
+                                     <br>
+                                    
+									<div class="centrar">
+                                    <br>
+									<a href="#" class="button1" onClick="document.getElementById('ContactForm').submit()">Registrar</a>
+									<a href="#" class="button1" onClick="document.getElementById('ContactForm').reset()">Cancelar</a>		
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div><br>
+                
+                <div class="centrar">
+						<section class="col-1-3"><div class="centrar">
+							<div id="contenido">
+							  <div class="box">
+                                <div class="centrar">
+                                  <li><a href="nuevoPlatillo.html"><img src="../images/Nuevo platillo.png" alt="" ></a></li>
+                                </div>
+							</div>
+						</div>
+						</div>
+                        </section>
+						<section class="col-1-3"><div class="centrar">
+							<div class="box">
+								<div class="centrar">
+								  <li><a href="nuevaSucursal.html"><img src="../images/Nueva sucursal.png" alt="" ></a></li>
+								</div>
+							</div>
+						</div></section>
+			  </div>
+              
+			</article>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+<div class="body3">
+
+		<div class="main zerogrid">
+		
+		<?php include '../plantillasphp/footRCC.php' ; ?>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
