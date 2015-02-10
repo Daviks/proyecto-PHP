907b304c6fbe0e230a6a1ed3fad2098f1fa36432
Carlos León <carlos.leon.lucas@gmail.com>
2015-02-10 11:02:40 -0500

    cambio en las extensiones del menu en restaurante.php

--- plantillasphp/bodyrestaurante.php
+++ plantillasphp/bodyrestaurante.php
@@ -5,25 +5,9 @@
 		<div class="body5">
 			<div class="main zerogrid">
 <!-- header -->
-				<header>
-					<h1><a href="../index.html" id="logo"><img src="../images/logoenblanco.png" alt="" ></a></h1>
-					<nav>
-						<ul id="top_nav">
-							<li><a href="../pages/ingresar.html" title="Ingresar"><img src="../images/icon_1.gif" alt=""></a></li>
-							<li><a href="../pages/registrar.html" title="Registrar"><img src="../images/icon_2.gif" alt=""></a></li>
-							<li class="end"><a href="../pages/ayuda.html" title="Ayuda"><img src="../images/icon_3.gif" alt=""></a></li>
-						</ul>
-					</nav>
-					<nav>
-						<ul id="menu">
-							<li class="active"><a href="#">Restaurante</a></li>
-							<li><a href="../pages/ciudad.html">Ciudad</a></li>
-							<li><a href="../pages/comida.html">Comida</a></li>
-                             <li><a href="../pages/nuevaMarca.html">Administración</a></li>
-                            <li><a href="../pages/nosotros.html">Nosotros</a></li>
-						</ul>
-					</nav>
-				</header>
+				
+				<?php include '../plantillasphp/headerRCC.php' ; ?>
+				
 				<h2 id="titulopag"> Cafeterías </h2> <input  href="#" class="button1" id="seccionbuscar" type="submit" value="Buscar"><br><br> <input  id="seccionbuscar" type="search" name="buscar"><br>
 				
 <!-- / header -->
