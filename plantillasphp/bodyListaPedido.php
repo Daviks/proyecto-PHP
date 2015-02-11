05a17950bc48f37b891e122bf729499d4aacbbb2
Arturo <arturonrg@gmail.com>
2015-02-11 17:51:40 -0500

    body para lista de pedido

---
+++ plantillasphp/bodyListaPedido.php
@@ -0,0 +1,48 @@
+
+
+<?php include '../plantillasphp/headerBodyListaPedido.php' ; ?>
+
+				<h2 id="titulopag"> Lista de Pedido </h2> 
+				<br>
+				<!--<input  href="#" class="button1" id="seccionbuscar" type="submit" value="Buscar"><br><br> <input  id="seccionbuscar" type="search" name="buscar"><br>
+				-->
+				<div id="contenido">
+				
+									<div id="banner_left">
+										<!--
+										<ul id="menu_categorias">
+											<li id="cate">Categor&iacuteas</li>
+											<li>Caf&eacute</li>
+											<li>Pizza</li>
+											<li>Mariscos</li>
+											<li>Carnes</li>
+											
+										</ul>
+                    -->
+									</div>
+									
+									<div id="banner_right">
+										<!--
+										<img src="../images/Tomate.png" class="imares"  alt="res1">
+										<img src="../images/mcdonalds.png" class="imares"  alt="res1" >
+										<img src="../images/Menestras_Del_Negro_Logo.jpg" class="imares"  alt="res1" >
+											-->	
+										<a rel="nofollow" href="../pages/xxxxx" >Confirmar pedido...</a>
+									</div>
+								  <br/> 
+				</div>
+	
+				
+<!-- / header -->
+<!-- content -->
+				<article id="content">							
+				</article>
+
+			</div>
+		</div>
+				</div>
+</div>
+<br>
+<?php include '../plantillasphp/footerBodyIngresar.php' ; ?>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
