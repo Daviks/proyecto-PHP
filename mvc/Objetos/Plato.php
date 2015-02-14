b304fa5d0f6a76c7fb24299006c28fbd88c44154
andresitoo <andymorale@hotmail.com>
2015-02-14 16:05:21 -0500

    agregacion del collector

--- mvc/Objetos/Plato.php
+++ mvc/Objetos/Plato.php
@@ -1,20 +1,20 @@
-<?
-include_once('Categoria.php');
+<?php
 
-class Plato extends Categoria
+class Plato 
 
 {
 	private $idPlato;
 	private $nombre;
 	private $precio;
 	private $descripcion;
+  private $categoria;
 
-
-	function __construct($idPlato, $nombre, $precio, $descripcion){
+	function __construct($idPlato, $nombre, $precio, $descripcion, $categoria){
 		$this->idPlato = $idPlato;
        	$this->nombre = $nombre;
        	$this->precio = $precio;
        	$this->descripcion = $descripcion;
+        $this->categoria = $categoria;
  
 	}
 	function setIdPlato($idPlato){
@@ -44,4 +44,12 @@ class Plato extends Categoria
      function getDescripcion(){
        return $this->descripcion;
      }
+     function setCategoria($categoria){
+       $this->categoria = $categoria;
+     } 
+     function getCategoria(){
+       return $this->categoria;
+     }
 }
+
+?> 
