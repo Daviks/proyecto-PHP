589de9b135fae4018017e9266ae20e5758cab042
Daviks <fedaloor_0605@hotmail.es>
2015-02-15 13:02:32 -0500

    Creación de objetos

---
+++ mvc/Objetos/Telefono.php
@@ -0,0 +1,78 @@
+<?php
+class Telefono
+{
+	private $idTelefono;
+	private $Secuencia;
+	private $Numero;
+	private $Tipo;
+	private $Descripcion;
+	private $idCliente;
+	private $idSucursal;
+	
+	function __constructor($idTelefono, $Secuencia, $Numero, $Tipo, $Descripcion, $idCliente, $idSucursal){
+		$this->idTelefono = $idTelefono;
+		$this->Secuencia = $Secuencia;
+		$this->Numero = $Numero;
+		$this->Tipo = $Tipo;
+		$this->Descripcion = $Descripcion;
+		$this->idCliente = $idCliente;
+		$this->idSucursal = $idSucursal;
+	}
+	
+	function setIdTelefono($idTelefono){
+		$this->idTelefono = $idTelefono;
+	}
+	
+	function setSecuencia($Secuencia){
+		$this->Secuencia = $Secuencia;
+	}
+	
+	function setNumero($Numero){
+		$this->Numero = $Numero;
+	}
+	
+	function setTipo($Tipo){
+		$this->Tipo = $Tipo;
+	}
+	
+	function setDescripción($Descripción){
+		$this->Descripción = $Descripción;
+	}
+	
+	function setIdCliente($idCliente){
+		$this->idCliente = $idCliente;
+	}
+	
+	function setIdSucursal($idSucursal){
+		$this->idSucursal = $idSucursal;
+	}
+	
+	function getIdTelefono(){
+		return $this->idTelefono;
+	}
+	
+	function getSecuencia(){
+		return $this->Secuencia;
+	}
+	
+	function getNumero(){
+		return $this->Numero;
+	}
+	
+	function getTipo(){
+		return $this->Tipo;
+	}
+	
+	function getDescripcion(){
+		return $this->Descripcion;
+	}
+	
+	function getIdCliente(){
+		return $this->idCliente;
+	}
+	
+	function getIdSucursal(){
+		return $this->idSucursal;
+	}
+}
+?>
