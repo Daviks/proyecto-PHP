5208373d41d184014499680cd82e75fadad92a74
JDGetsuga <jdavid_wolf@hotmail.com>
2015-03-02 00:58:20 -0500

    arreglo de nombre base

--- mvc/collectorDeObjetos/Collector.php
+++ mvc/collectorDeObjetos/Collector.php
@@ -3,8 +3,8 @@ include_once('dataBase.php');
 // Define configuration
 define("DB_HOST", "127.0.0.1");
 define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "crazzyfood");
+define("DB_PASS", "estudiante");
+define("DB_NAME", "CrazzyFood");
 
 
 
